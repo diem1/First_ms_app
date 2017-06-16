b043bdf11e2900db86339f82ebc7a95497274d6f
Dmytro Soroka <diem.3344@gmail.com>
2017-06-16 12:19:09 -0700

    display the all employees of company

--- First_ms_app/Default.aspx.cs
+++ First_ms_app/Default.aspx.cs
@@ -18,50 +18,84 @@ namespace First_ms_app
         protected void Page_Load(object sender, EventArgs e)
         {
             string ConnectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
-
             SqlConnection SqlConnection = new SqlConnection(ConnectionString);
-            SqlCommand Cmd = new SqlCommand();
-            SqlDataReader DataReader;
 
-            Cmd.CommandText = @"SELECT * FROM Companies INNER JOIN Addresses 
+            SqlCommand ReadCompaniesCmd = new SqlCommand();            
+            SqlDataReader CompaniesDataReader;
+            
+
+            ReadCompaniesCmd.CommandText = @"SELECT * FROM Companies INNER JOIN Addresses 
                                 ON Companies.Address = Addresses.Address_ID INNER JOIN Employees 
                                 ON Companies.Agent = Employees.Employee_ID";
-            Cmd.Connection = SqlConnection;
+            ReadCompaniesCmd.Connection = SqlConnection;
+            
             SqlConnection.Open();
-            DataReader = Cmd.ExecuteReader();
+            CompaniesDataReader = ReadCompaniesCmd.ExecuteReader();          
 
             DataTable CompaniesDataTable = new DataTable();
             CompaniesDataTable.Columns.Add("ID", Type.GetType("System.String"));
             CompaniesDataTable.Columns.Add("Name", Type.GetType("System.String"));
             CompaniesDataTable.Columns.Add("Address", Type.GetType("System.String"));
             CompaniesDataTable.Columns.Add("Agent", Type.GetType("System.String"));
+            CompaniesDataTable.Columns.Add("Employees", Type.GetType("System.String"));
 
-            while (DataReader.Read())
+            while (CompaniesDataReader.Read())
             {
                 CompaniesDataTable.Rows.Add();
                 int RowNumber = CompaniesDataTable.Rows.Count - 1;
-                CompaniesDataTable.Rows[RowNumber]["ID"] = DataReader["Company_ID"].ToString();
-                CompaniesDataTable.Rows[RowNumber]["Name"] = DataReader["Name"].ToString();
+                string CompanyID = CompaniesDataReader["Company_ID"].ToString();
+                string CompanyName = CompaniesDataReader["Name"].ToString();
 
-                string Address = DataReader["Address_1"].ToString() + ", " + DataReader["Address_2"].ToString();
+                CompaniesDataTable.Rows[RowNumber]["ID"] = CompanyID;
+                CompaniesDataTable.Rows[RowNumber]["Name"] = CompanyName;
+
+                string Address = CompaniesDataReader["Address_1"].ToString() + ", "
+                                    + CompaniesDataReader["Address_2"].ToString();
                 CompaniesDataTable.Rows[RowNumber]["Address"] = Address;
 
-                string AgentFirstName = DataReader["First_name"].ToString();
-                string AgentLastName = DataReader["Last_name"].ToString();
-                string AgentPhoneNumber = DataReader["Phone_Number"].ToString();
-                string AgentSpecialization = DataReader["Specialization"].ToString();
+                string AgentFirstName = CompaniesDataReader["First_name"].ToString();
+                string AgentLastName = CompaniesDataReader["Last_name"].ToString();
+                string AgentPhoneNumber = CompaniesDataReader["Phone_Number"].ToString();
+                string AgentSpecialization = CompaniesDataReader["Specialization"].ToString();
                 string Agent = AgentFirstName + " " 
                         + AgentLastName + ", "
                         + AgentPhoneNumber + ", " 
                         + AgentSpecialization + ", ";
                 CompaniesDataTable.Rows[RowNumber]["Agent"] = Agent;
             }
+            SqlConnection.Close();
 
+            // Read Employeers
+            SqlCommand ReadEmployeesCmd = new SqlCommand();
+            SqlDataReader EmployeesDataReader;
+            ReadEmployeesCmd.CommandText = @"SELECT * FROM Companies 
+                                JOIN Companies_Employees ON Company_ID=Companies_Employees.Company
+                                JOIN Employees ON Companies_Employees.Employee=Employee_ID";
+            ReadEmployeesCmd.Connection = SqlConnection;
+            SqlConnection.Open();
+            EmployeesDataReader = ReadEmployeesCmd.ExecuteReader();
+            while (EmployeesDataReader.Read())
+            {
+                string EmployeeCompanyID = EmployeesDataReader["Company"].ToString();
+                string EmployeeFirstName = EmployeesDataReader["First_name"].ToString();
+                string EmployeeLastName = EmployeesDataReader["Last_name"].ToString();
+                string EmployeePhoneNumber = EmployeesDataReader["Phone_number"].ToString();
+                string EmaployeeSpecialization = EmployeesDataReader["Specialization"].ToString();
+                foreach (DataRow Row in CompaniesDataTable.Rows)
+                {
+                    if (EmployeeCompanyID.ToString() == Row["ID"].ToString())
+                    {
+                        Row["Employees"] += EmaployeeSpecialization + ", "
+                                            + EmployeeFirstName + " "
+                                            + EmployeeLastName + ", "
+                                            + EmployeePhoneNumber + " | ";
+                    }
+                }
+            }
             SqlConnection.Close();
 
             CompaniesGridView.DataSource = CompaniesDataTable;
             CompaniesGridView.DataBind();
-
         }
     }
 }
